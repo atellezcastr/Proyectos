5a01ab334fea4d0064509ef509a9d8abae00f958
Christian Tellez <atellezcastr@mun.ca>
2021-08-03 20:06:40 -0230

    Splitting code

--- reactTutorial/src/App.js
+++ reactTutorial/src/App.js
@@ -1,26 +1,25 @@
-import { StrictMode, useState } from "react";
-import ReactDOM from "react-dom";
+import { useState, StrictMode } from "react";
+import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import SearchParams from "./SearchParams";
 import Details from "./Details";
+import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
-      <div 
+      <div
         className="p-0 m-0"
         style={{
-          background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+          background:
+            "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
         }}
       >
         <Router>
-          <header
-            className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500"
-          >
-            <Link to="/" className="text-6xl text-white hover:text-gray-200">
-              <h1>Try me!</h1>
+          <header className="w-full mb-10 bg-gradient-to-b text-center p-7 from-purple-400 via-pink-500 to-red-500">
+            <Link className="text-6xl text-white hover:text-gray-200" to="/">
+              Adopt Me!
             </Link>
           </header>
           <Switch>
@@ -36,7 +35,8 @@ const App = () => {
     </ThemeContext.Provider>
   );
 };
-ReactDOM.render(
+
+render(
   <StrictMode>
     <App />
   </StrictMode>,
--- reactTutorial2/src/SearchParams.js
+++ reactTutorial2/src/SearchParams.js
@@ -27,29 +27,25 @@ const SearchParams = () => {
   }
 
   return (
-    <div className="my-0 mx-auto w-11/12">
+    <div className="search-params">
       <form
-        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center divide-y divide-gray-900"
         onSubmit={(e) => {
           e.preventDefault();
           requestPets();
         }}
       >
-        <label className="search-label" htmlFor="location">
+        <label htmlFor="location">
           Location
           <input
-            className="w-60"
-            type="text"
             id="location"
             value={location}
             placeholder="Location"
             onChange={(e) => updateLocation(e.target.value)}
           />
         </label>
-        <label className="search-label" htmlFor="animal">
+        <label htmlFor="animal">
           Animal
           <select
-            className="w-60"
             id="animal"
             value={animal}
             onChange={(e) => updateAnimal(e.target.value)}
@@ -63,10 +59,9 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <label className="search-label" htmlFor="breed">
+        <label htmlFor="breed">
           Breed
           <select
-            className="w-60 disabled:opacity-50"
             disabled={!breeds.length}
             id="breed"
             value={breed}
@@ -81,10 +76,9 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <label className="search-label" htmlFor="theme">
+        <label htmlFor="theme">
           Theme
           <select
-            className="w-60"
             value={theme}
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
@@ -95,12 +89,7 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button
-          className="rounded px-6 py-2 color text-white hover:opacity-50 border-none"
-          style={{ backgroundColor: theme }}
-        >
-          Submit
-        </button>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
